88244fa3e1f79e414df9758a93a05cee3164e03e
Aadhithya Prakash <aadhithyasubha2018@gmail.com>
2025-03-09 09:51:21 +0530

    canteen project

---
+++ components/footer.php
@@ -0,0 +1,29 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">shaikhanas@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      
+
+      
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Sara's Digital Cafeteria</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
